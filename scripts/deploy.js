e63cd7c1b04413bef4da5544e0f8ffdae32e0bb6
Yash-Chandrani <yashbeskkc@gmail.com>
2022-04-07 21:42:33 +0530

    deploy locally

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const main = async() => {
+    const [deployer] = await hre.ethers.getSigners();
+    const accountBalance = await deployer.getBalance();
+
+    console.log("Deploying contracts with the account :", deployer.address);
+    console.log("Account balance: ", accountBalance.toString());
+
+    const commContractFactory = await hre.ethers.getContractFactory("CommPortal");
+    const commContract = await commContractFactory.deploy();
+    await commContract.deployed();
+
+    console.log("CommPortal address: ", commContract.address);
+};
+
+const runMain = async() => {
+    try {
+        await main();
+        process.exit(0); //exit Node process without error
+    } catch (error) {
+        console.log(error);
+        process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+    }
+};
+
+runMain();
